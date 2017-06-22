70137a38834af4260118d36dfbba5a3a47855100
Federico Cerchiari <federicocerchiari@gmail.com>
2017-06-22 17:26:58 +0200

    Update test_tags.py

--- tests/test_tags.py
+++ tests/test_tags.py
@@ -14,9 +14,20 @@ class TestTag(unittest.TestCase):
         self.page = Html()
 
     def is_tag(self, tag):
+        """a tag should be instance of Tag and DOMElement, and should have a TagAttrs attribute)"""
         self.assertIsInstance(tag, Tag)
         self.assertIsInstance(tag, DOMElement)
         self.assertIsInstance(tag.attrs, TagAttrs)
+
+    def check_head_body(self, head, body):
+        self.is_tag(head)
+        self.is_tag(body)
+        self.assertEqual(len(self.page.childs), 2)
+        self.assertEqual(self.page.length, 2)
+        self.assertEqual(self.page.childs[0], head)
+        self.assertEqual(self.page.childs[1], body)
+        self.assertEqual(self.page.first(), head)
+        self.assertEqual(self.page.last(), body)
 
     def test_create_instantiation(self):
         self.is_tag(self.page)
@@ -35,28 +46,23 @@ class TestTag(unittest.TestCase):
         head = Head()
         body = Body()
         self.page(head, body)
-        self.is_tag(head)
-        self.is_tag(body)
-        self.assertEqual(len(self.page.childs), 2)
-        self.assertEqual(self.page.length, 2)
-        self.assertEqual(self.page.childs[0], head)
-        self.assertEqual(self.page.childs[1], body)
-        self.assertEqual(self.page.first(), head)
-        self.assertEqual(self.page.last(), body)
+        self.check_head_body(head, body)
 
-    def test_create_call_iterable(self):
-        self.page([Head(), Body()])
-        divs = [Div() for _ in range(10)]
-        print(self.page.childs)
-        self.page.childs[1](divs)
-        body = self.page.childs[1]
-        self.is_tag(self.page.childs[1][1])
-        self.assertEqual(len(self.page.childs), self.page.length)
-        self.assertEqual(self.page.length, 2)
-        self.assertEqual(len(body.childs), body.length)
-        self.assertEqual(body, 10)
-        self.assertIsInstance(self.page.childs[0][0], Div)
+    def test_create_call_list(self):
+        l = [Head(), Body()]
+        self.page(l)
+        self.check_head_body(*l)
 
+    def test_create_call_tuple(self):
+        t = (Head(), Body())
+        self.page(t)
+        self.check_head_body(*t)
+
+    def test_create_call_generator(self):
+        g = (t for t in [Head(), Body()])
+        self.page(g)
+        head, body = self.page.childs
+        self.check_head_body(head, body)
 
 if __name__ == '__main__':
     unittest.main()
