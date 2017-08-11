f627b97237b72b0a88ab6e019f9c401d9f8fde7d
Federico Cerchiari <federicocerchiari@gmail.com>
2017-08-11 12:02:31 +0200

    Delete prepare_selector.py

--- prepare_selector.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import re
-
-separators = ['~', ',', '+', '>', ' ']
-regexPattern = '|'.join(map(re.escape, separators))
-selector = '#someId > div.someClass'
-spl = re.split(regexPattern, selector)
-
-print spl
