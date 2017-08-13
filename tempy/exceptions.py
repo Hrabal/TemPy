085568609e512a5bcf65dfd4101a6bce8a14121a
Hrabal <federicocerchiari@gmail.com>
2017-08-13 19:02:52 +0200

    Basic Exception structure [skip ci]

--- tempy/exceptions.py
+++ tempy/exceptions.py
@@ -4,5 +4,23 @@
 """
 
 
-class TagError(Exception):
-    pass
+class TempyException(Exception):
+    """Base TemPy exception class"""
+
+
+class TagError(TempyException):
+    """Base Tag Exception"""
+    def __init__(self, tag, *args):
+        super().__init__(*args)
+        self.tag = tag
+
+
+class ContentError(object):
+    """Raised when dealing with Content or DOMElement.content_data"""
+    def __init__(self, tag, content, *args):
+        super().__init__(tag, content, *args)
+        self.content = content
+
+
+class WrongContentError(ContentError, ValueError):
+    """Raised when the provided content is not a dict."""
