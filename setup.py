e6141930ce051d5a1bf98ebb45d94df6d7ec72ff
Hrabal <federicocerchiari@gmail.com>
2017-08-13 07:46:53 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+#   Copyright 2017 Federico Cerchiari <federicocerchiari@gmail.com>
+#
+#   this file is part of tempy
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+from setuptools import setup
+
+setup(name='tempy',
+      version='0.1',
+      author='Federico Cerchiari',
+      author_email='federicocerchiari@gmail.com',
+      description='Python OOP Templating System',
+      long_description=open('README.md').read(),
+      license='LICENSE',
+      packages=['tempy'],
+      url='https://hrabal.github.io/TemPy/'
+      )
