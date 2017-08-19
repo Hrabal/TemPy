453a8d1ac340b1c7767f5d9c2b85733f08ee75ac
Hrabal <federicocerchiari@gmail.com>
2017-08-19 09:37:09 +0200

    more benchmarks [skip ci]

--- benchmarks/bench_server.py
+++ benchmarks/bench_server.py
@@ -2,12 +2,20 @@
 import json
 from flask import Flask, render_template
 
-app = Flask(__name__)
+from tempy_templates.hello_world import page as hw_page
+from tempy_templates.sw_multifile import page as sw_multi_page
+from tempy_templates.sw_monofile import page as sw_mono_page
+from tempy_templates.sw_multifile_table import page as sw_multi_page_table
 
+app = Flask(__name__)
 with open('sw-people.json', 'r') as f:
     people = json.load(f)
 
-@app.route('/tempy_sw_monofile')
+
+@app.route('/j2_sw_multifile_table')
+def j2_sw_multifile_table():
+    return render_template('sw_characters_table.html', people=people)
+
 def tempy_sw():
     from tempy_templates.sw_monofile import page
     return page.render(characters=people.values())
@@ -16,10 +24,13 @@ def tempy_sw():
 def j2_sw():
     return render_template('j2_sw_monofile.html', people=people)
 
+@app.route('/tempy_sw_mono')
+def tempy_sw_monofile():
+    return sw_mono_page.render(characters=people.values())
+
 @app.route('/tempy_sw_multifile')
 def tempy_sw_multifile():
-    from tempy_templates.sw_multifile import page
-    return page.render(characters=people.values())
+    return sw_multi_page.render(characters=people.values())
 
 @app.route('/j2_sw_multifile')
 def j2_sw_multifile():
@@ -27,8 +38,8 @@ def j2_sw_multifile():
 
 @app.route('/tempy_hw')
 def tempy_hw():
-    from tempy_templates.hello_world import page
-    return page.render()
+    hw_page.body(a)
+    return hw_page.render()
 
 @app.route('/j2_hw')
 def j2_hw():
