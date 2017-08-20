60b24f658d957d5490469264911b0547b878b02c
Hrabal <federicocerchiari@gmail.com>
2017-08-20 12:08:53 +0200

    fix [skip ci]

--- setup.py
+++ setup.py
@@ -15,18 +15,27 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-
+import os
 from setuptools import setup
+
+long_description = """
+Build HTML without writing a single tag.
+TemPy dynamically generates HTML and accesses it in a pure Python or jQuery fashion.
+Navigating the DOM and manipulating tags is also possible in a Python or jQuery-similar sintax.
+
+No parsing and a simple structure makes TemPy fast. TemPy simply adds html tags around your data, and the actual html string exists only at render time.
+"""
 
 setup(name='tem-py',
       version='0.1.2',
       author='Federico Cerchiari',
       author_email='federicocerchiari@gmail.com',
       description='Python OOP Templating System',
-      license='LICENSE',
+      license='APACHE 2.0',
       packages=['tempy'],
-      url='https://hrabal.github.io/TemPy/',
+      url='https://github.com/Hrabal/TemPy',
       keywords=['python3', 'templating', 'html', 'web'],
-      download_url='https://github.com/Hrabal/TemPy/archive/0.1.tar.gz',
+      download_url='https://github.com/Hrabal/TemPy/archive/0.1.2.tar.gz',
       python_requires='>=3.3',
+      long_description=long_description
       )
