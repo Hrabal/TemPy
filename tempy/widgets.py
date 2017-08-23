e98519b70f8ba5a373495416d130aa5a7c096838
Federico Cerchiari <federicocerchiari@gmail.com>
2017-08-23 12:20:40 +0200

    Docstrings [skip ci]

--- tempy/widgets.py
+++ tempy/widgets.py
@@ -14,10 +14,15 @@ from .tags import Html, Table, Tbody, Thead, Caption, Tfoot, Td, Tr, Th, Li, Ul
 
 class TempyPage(Html):
     """HTML Page widget.
-    Builds an empty html page with named common tags:
+    Builds an empty html page with some named common tags:
     - Doctype
     - Head - Title - description meta - keywords meta
     - Body
+    Those elements are accessible as page attribute:
+    >>> TempyPage.head.charset
+    >>> TempyPage.body
+    >>> TempyPage.head.title
+    Provides an API to manage common meta tags directly.
     """
     __tag = Html._Html__tag
 
@@ -36,14 +41,17 @@ class TempyPage(Html):
         )
 
     def set_charset(self, charset):
+        """Changes the <meta> charset tag (default charset in init is UTF-8)."""
         self.head.charset.attr(charset=charset)
         return self
 
     def set_description(self, description):
+        """Changes the <meta> description tag."""
         self.head.description.attr(content=description)
         return self
 
     def set_keywords(self, keywords):
+        """Changes the <meta> keywords tag."""
         self.head.keywords.attr(content=''.join(keywords))
         return self
 
@@ -155,19 +163,22 @@ class TempyTable(Table):
         return cell if tags else cell.childs[0]
 
     def make_header(self, head):
+        """Makes the header row from the given data."""
         if not hasattr(self, 'header'):
             self.header = self(header=Thead())
         return self.header(Tr()(Th()(col) for col in head))
 
     def make_footer(self, footer):
+        """Makes the footer row from the given data."""
         if not hasattr(self, 'footer'):
             self.footer = self(footer=Tfoot())
         return self.footer(Tr()(Td()(col) for col in footer))
 
     def make_caption(self, caption):
+        """Adds/Sobstitute the table's caption."""
         if not hasattr(self, 'caption'):
             self(caption=Caption())
-        return self.caption(caption)
+        return self.caption.empty()(caption)
 
 
 class TempyListMeta:
@@ -196,6 +207,7 @@ class TempyListMeta:
 
 
 class TempyList:
+    """TempyList is a general Class factory, 
     def __new__(cls, typ=None, struct=None):
         if isinstance(typ, str):
             try:
