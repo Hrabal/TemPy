86060bc1df3615f262d9c0c6ad47d405626a722d
Federico Cerchiari <federicocerchiari@gmail.com>
2017-10-19 11:48:34 +0200

    [skip ci]

--- examples/templates/static_files.py
+++ examples/templates/static_files.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from flask import url_for
 from tempy import Html, Head, Title, Body, P, Img
 
 page = Html()(
@@ -9,6 +10,9 @@ page = Html()(
     ),
     body=Body()(
             P()("Here's a static picture of a cat!"),
-            Img(src='./static/cat.jpg')
+            Img(src='./static/cat.jpg'),
+            Br(),
+            P()("Here's a static picture of the same cat, but found using flask's url_for, WHOAH!!"),
+            Img(src=url_for('static', filename='cat.jpg'))
         )
     )
