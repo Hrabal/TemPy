3a4f46a652a892be9292a89e8d83aa0cc8c9a5cf
Federico Cerchiari <federicocerchiari@gmail.com>
2017-10-26 17:31:50 +0200

    Update __init__.py

--- tempy/__init__.py
+++ tempy/__init__.py
@@ -1,5 +1,6 @@
 from .tags import *
 from .tempy import Content, Css, render_template, TagAttrs, TempyREPR, Escaped
+frmo .tempygod import T
 
 __version__ = '0.5.4'
 VERSION = tuple(map(int, __version__.split('.')))
