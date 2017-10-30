845259ba0b84264a8af752d77466a482837784d3
Federico Cerchiari <federicocerchiari@gmail.com>
2017-10-30 15:23:17 +0100

    Added T.from_string method.

--- tempy/t.py
+++ tempy/t.py
@@ -1,6 +1,49 @@
 # -*- coding: utf-8 -*-
 # @author: Federico Cerchiari <federicocerchiari@gmail.com>
+import importlib
+from html.parser import HTMLParser
 from .tempy import Tag, VoidTag
+
+class TempyParser(HTMLParser):
+
+    def __init__(self):
+        super().__init__()
+        self.result = []
+        self.current_tag = None
+        self.unknown_tag_maker = TempyFactory()
+        self.tempy_tags = importlib.import_module('.tags', package='tempy')
+
+    def _make_tempy_tag(self, tag, attrs, void):
+        tempy_tag_cls = getattr(self.tempy_tags, tag.title(), None)
+        if not tempy_tag_cls:
+            unknow_maker = [self.unknown_tag_maker, self.unknown_tag_maker.Void][void]
+            tempy_tag_cls = unknow_maker[tag]
+        tempy_tag = tempy_tag_cls(**dict(attrs))
+        if not self.current_tag:
+            self.result.append(tempy_tag)
+            if not void:
+                self.current_tag = tempy_tag
+        else:
+            self.current_tag(tempy_tag)
+            self.current_tag = self.current_tag.childs[-1]
+
+    def handle_starttag(self, tag, attrs):
+        self._make_tempy_tag(tag, attrs, False)
+
+    def handle_startendtag(self, tag, attrs):
+        self._make_tempy_tag(tag, attrs, True)
+
+    def handle_endtag(self, tag):
+        self.current_tag = self.current_tag.parent
+
+    def handle_data(self, data):
+        self.current_tag(data)
+
+    def handle_comment(self, data):
+        pass
+
+    def handle_decl(self, decl):
+        pass
 
 
 class TempyFactory:
@@ -25,4 +68,15 @@ class TempyFactory:
         return tag
 
 
-T = TempyFactory()
+class TempyGod(TempyFactory):
+
+    def __init__(self):
+        super().__init__()
+        self._parser = TempyParser()
+
+    def from_string(self, html_string):
+        self._parser.feed(html_string)
+        return self._parser.result
+
+
+T = TempyGod()
