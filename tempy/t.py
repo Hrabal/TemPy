77fe754245d1b4afcacf635c284e484f6df924dd
Federico Cerchiari <federicocerchiari@gmail.com>
2017-10-30 16:04:10 +0100

    Update t.py

--- tempy/t.py
+++ tempy/t.py
@@ -5,6 +5,13 @@ from html.parser import HTMLParser
 from .tempy import Tag, VoidTag
 
 class TempyParser(HTMLParser):
+    """Custom parser used to translate an html into Tempy Tags.
+    See https://docs.python.org/3/library/html.parser.html for details on how parsing is performed.
+    Every tag found by the parser is converted into a TempyTag, every subsequent element will be added
+    inside this one.
+    As a result of this, unclosed tags in imput will be closed in the resulting Tempy Tree right before the parent element is closed.
+    This behaviour is accidental and should not be used a s a html sanitizing feature.
+    """
 
     def __init__(self):
         super().__init__()
@@ -14,6 +21,7 @@ class TempyParser(HTMLParser):
         self.tempy_tags = importlib.import_module('.tags', package='tempy')
 
     def _make_tempy_tag(self, tag, attrs, void):
+        """Searches in tempy.tags for the correct tag to use, if does not exists uses the TempyFacotry to create a custom tag."""
         tempy_tag_cls = getattr(self.tempy_tags, tag.title(), None)
         if not tempy_tag_cls:
             unknow_maker = [self.unknown_tag_maker, self.unknown_tag_maker.Void][void]
@@ -75,6 +83,7 @@ class TempyGod(TempyFactory):
         self._parser = TempyParser()
 
     def from_string(self, html_string):
+        """Parses an html string and returns a list of Tempy trees."""
         self._parser.feed(html_string)
         return self._parser.result
 
