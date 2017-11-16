7995be8f846c0aa8338fa0f3bc01aa3e3a21a6b8
Hrabal <federicocerchiari@gmail.com>
2017-11-16 18:56:11 +0100

    Added RuntimError for unsupported python versions.

--- tempy/__init__.py
+++ tempy/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # @author: Federico Cerchiari <federicocerchiari@gmail.com>
+import sys
 from .tools import render_template
 from .elements import Tag, VoidTag, Css, Content
 from .tempyrepr import TempyREPR
@@ -9,3 +10,6 @@ from .t import T
 
 __version__ = '1.0.0'
 VERSION = tuple(map(int, __version__.split('.')))
+
+if sys.version_info < (3, 3):
+    raise RuntimeError('You need Python 3.3+ for this module.')
