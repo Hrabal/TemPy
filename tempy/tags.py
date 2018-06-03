bb49719cd87ffecf1235d733948843f3ee89a274
Hrabal <federicocerchiai@gmail.com>
2018-06-03 11:57:14 +0200

    Minor pep8

--- tempy/tags.py
+++ tempy/tags.py
@@ -102,17 +102,18 @@ class Title(Tag):
         super().__init__()
         self(title)
 
+
 tags = {
     VoidTag: ['wbr', 'area', 'base', 'br', 'embed', 'img', 'hr', 'input', 'link', 'meta', 'param', 'source', 'track'],
-    Tag: ['abbr', 'acronym', 'address', 'applet', 'article', 'aside', 'audio', 'b', 'basefont', 'bdi', 'bdo', 'big', 
+    Tag: ['abbr', 'acronym', 'address', 'applet', 'article', 'aside', 'audio', 'b', 'basefont', 'bdi', 'bdo', 'big',
           'blockquote', 'body', 'button', 'canvas', 'caption', 'center', 'cite', 'code', 'colgroup', 'datalist', 'dd',
           'del', 'details', 'dfn', 'dialog', 'dir', 'div', 'dl', 'dt', 'em', 'fieldset', 'figcaption', 'figure',
           'font', 'footer', 'form', 'frame', 'frameset', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'head', 'header', 'i',
           'iframe', 'ins', 'kbd', 'keygen', 'label', 'legend', 'li', 'main', 'map', 'mark', 'menu', 'menuitem', 'meter',
           'nav', 'noframes', 'noscript', 'object', 'ol', 'optgroup', 'option', 'output', 'p', 'picture', 'pre',
           'progress', 'q', 'rp', 'rt', 'ruby', 's', 'samp', 'script', 'section', 'select', 'small', 'span', 'strike',
-          'strong', 'style', 'sub', 'summary', 'sup', 'table', 'tbody', 'td', 'textarea', 'tfoot', 'th', 'thead', 'time',
-          'tr', 'tt', 'u', 'ul', 'var', 'video', ]
+          'strong', 'style', 'sub', 'summary', 'sup', 'table', 'tbody', 'td', 'textarea', 'tfoot', 'th', 'thead',
+          'time', 'tr', 'tt', 'u', 'ul', 'var', 'video', ]
 }
 
 
