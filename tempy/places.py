c0cfe64b6c6de7a0c146481b6b4d20845222493d
Hrabal <federicocerchiai@gmail.com>
2018-06-09 12:09:02 +0200

    Minor pep8 [skip ci]

--- tempy/places.py
+++ tempy/places.py
@@ -10,6 +10,7 @@ from .tempyrepr import TempyPlace
 
 class Inside(TempyPlace):
     """Check if a TempyREPR object's container is inside a certain Tempy Tag"""
+
     def _reprscore_container_parent(self, container, child):
         if container.parent.__class__ == self._pointer_class:
             return 1
@@ -21,8 +22,8 @@ class Sibling(TempyPlace):
     @classmethod
     def _check_container_siblings(cls, container, start=-1, stop=2):
         container_index = container.parent.childs.index(container)
-        before = container.parent.childs[container_index+start:container_index]
-        after = container.parent.childs[container_index+1:container_index+stop]
+        before = container.parent.childs[container_index + start:container_index]
+        after = container.parent.childs[container_index + 1:container_index + stop]
         for sibling in before + after:
             if sibling.__class__ == cls._pointer_class:
                 return 1
@@ -31,18 +32,21 @@ class Sibling(TempyPlace):
 
 class Near(Sibling):
     """Check if a TempyREPR object's container if near a certain Tempy Tag"""
+
     def _reprscore_container_siblings(self, container, child):
         return self._check_container_siblings(container)
 
 
 class Before(Sibling):
     """Check if a TempyREPR object's container if before a certain Tempy Tag"""
+
     def _reprscore_container_following(self, container, child):
         return self._check_container_siblings(container, start=0)
 
 
 class After(Sibling):
     """Check if a TempyREPR object's container if after a certain Tempy Tag"""
+
     def _reprscore_container_previous(self, container, child):
         return self._check_container_siblings(container, stop=0)
 
@@ -54,6 +58,7 @@ for tag in dir(all_tags):
         for place_type in (Inside, Near, Before, After):
             place_tag_name = '%s%s' % (place_type.__name__, tag)
             # Dynamic class definition
-            place_cls = type(place_tag_name, (place_type, ), {'_pointer_class': getattr(all_tags, tag), '_base_place': False})
+            place_cls = type(place_tag_name, (place_type, ),
+                             {'_pointer_class': getattr(all_tags, tag), '_base_place': False})
             # We put the new dynamically created class inside locals to make it avaiable from the outside
             locals()[place_tag_name] = place_cls
