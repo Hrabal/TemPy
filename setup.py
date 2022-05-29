44af773e7625eb0ee546b6a89ca355d8b1a5d1ea
Hrabal <federicocerchiari@gmail.com>
2022-05-29 12:27:12 +0200

    better setup.py

--- setup.py
+++ setup.py
@@ -15,29 +15,92 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-from setuptools import setup
+import os
+import sys
+from shutil import rmtree
+from pathlib import Path
+from setuptools import find_packages, setup, Command
 
 from tempy import __version__
 
-long_description = """
-Build HTML without writing a single tag.
-TemPy dynamically generates HTML and accesses it in a pure Python or jQuery fashion.
-Navigating the DOM and manipulating tags is also possible in a Python or jQuery-similar sintax.
+# Package meta-data.
+NAME = "tem-py"
+DESCRIPTION = "Python OOP Templating System"
+URL = "https://github.com/Hrabal/TemPy"
+EMAIL = "federicocerchiari@gmail.com"
+AUTHOR = "Federico Cerchiari"
+REQUIRES_PYTHON = ">=3.3"
+VERSION = __version__
 
-No parsing and a simple structure makes TemPy fast. TemPy simply adds html tags around your data, and the actual html string exists only at render time.
-"""
+REQUIRED = ["mistune", ]
+EXTRAS = {}
 
-setup(name='tem-py',
+
+here = os.path.abspath(os.path.dirname(__file__))
+long_description = (Path(__file__).parent / "README.md").read_text()
+
+
+class UploadCommand(Command):
+      """Support setup.py upload."""
+
+      description = "Build and publish the package."
+      user_options = []
+
+      @staticmethod
+      def status(s):
+            print(f"\033[1m{s}\033[0m")
+
+      def initialize_options(self):
+            pass
+
+      def finalize_options(self):
+            pass
+
+      def run(self):
+            try:
+                  self.status("Removing previous builds…")
+                  rmtree(os.path.join(here, "dist"))
+            except OSError:
+                  pass
+
+            self.status("Building Source and Wheel (universal) distribution…")
+            os.system(f"{sys.executable} setup.py sdist bdist_wheel --universal")
+
+            self.status("Uploading the package to PyPI via Twine…")
+            os.system("twine upload dist/*")
+
+            self.status("Pushing git tags…")
+            os.system(f"git tag v{__version__}")
+            os.system("git push --tags")
+
+            sys.exit()
+
+
+# Where the magic happens:
+setup(
+      name=NAME,
       version=__version__,
-      author='Federico Cerchiari',
-      author_email='federicocerchiari@gmail.com',
-      description='Python OOP Templating System',
-      license='APACHE 2.0',
-      packages=['tempy', 'tempy.widgets'],
-      url='https://github.com/Hrabal/TemPy',
-      keywords=['python3', 'templating', 'html', 'web'],
-      download_url='https://github.com/Hrabal/TemPy/archive/%s.tar.gz' % __version__,
-      python_requires='>=3.3',
+      description=DESCRIPTION,
       long_description=long_description,
-      install_requires=['mistune', ],
-      )
+      long_description_content_type="text/markdown",
+      author=AUTHOR,
+      author_email=EMAIL,
+      python_requires=REQUIRES_PYTHON,
+      url=URL,
+      packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+      install_requires=REQUIRED,
+      extras_require=EXTRAS,
+      include_package_data=True,
+      license="APACHE 2.0",
+      classifiers=[
+            "License :: OSI Approved :: Apache Software License",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: Implementation :: CPython",
+            "Programming Language :: Python :: Implementation :: PyPy",
+            "Topic :: Internet :: WWW/HTTP :: HTTP Servers",
+            "Topic :: Internet :: WWW/HTTP :: WSGI :: Server",
+      ],
+      cmdclass={"upload": UploadCommand,},
+)
